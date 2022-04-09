fa8c4f30b6ed5cc5195d7f66b1e297a55b0e2f2b
alaska1997 <lsendoya@unal.edu.co>
2022-04-08 19:29:32 -0500

    ejercicio realizado

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const food = [{
+        name: "burger",
+        price: 8300,
+    }, {
+        name: "pizza",
+        price: 3200,
+    },
+    {
+        name: "hotdog",
+        price: 4500,
+    }
+];
+
+const ul = document.querySelector("#list");
+food.forEach(product => {
+    ul.innerHTML += `<ul>${product.name} - ${product.price}</ul>\n `
+})
